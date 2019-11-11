f2af43b70d17afe7fa842f1f455e68c42bd96299
CCulver831 <c.culver831@gmail.com>
2019-11-11 15:07:22 -0800

    Working onPt.2

--- src/Part2Test.java
+++ src/Part2Test.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -6,27 +8,37 @@ public class Part2Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		  String file = null;
-		  int len;
+		  File file = new File("AliceInWonderLand.txt");
+		 // int len;
 		  int count = 0;
 		  String[] words;
-		  String[] unrepeatedWords;
-		  Scanner in = new Scanner(file);
+		  //String[] unrepeatedWords;
+		  Scanner in = null;
+		try {
+			in = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		 Map<String, Integer> record = new HashMap<String, Integer>();
 		while (in.hasNextLine()) {
 		    String line = in.nextLine();
 		    words = line.split(" ");
 
-		    //here so it can use the split from the previous line
+		   
 		    for (String word : words) {
 		        if (!record.containsKey(word)) {
 		            record.put(word, 1);
+	                 if (word.equalsIgnoreCase("the"))   
+	                 {
+	                   count++;  
+	                 }
 		        }
 		        else {
 		            record.put(word, record.get(word) + 1);
 		        }
 		    }
 	}
-
+			System.out.println("Count: " + count);
 }
 }
